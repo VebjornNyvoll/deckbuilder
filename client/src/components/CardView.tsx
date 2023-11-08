e79975b984e2b2b57aa8e8bbe37e8a9dda37437b
Sigve Johan Hov <sigvejh@stud.ntnu.no>
2023-11-08 11:58:18 +0100

    [Type]: Fix overflow issues [Optional Body explaining the change in more detail]: Implemented a quick fix for how scrolling worked on phone and extra scrollbar on 1080p screens [References or Issue Numbers]: #12

--- client/src/components/CardView.tsx
+++ client/src/components/CardView.tsx
@@ -1,34 +1,20 @@
 import { useState, useEffect, useRef } from 'react';
 import { CardService } from '../service/CardService';
-import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
-import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown';
+import { DataView } from 'primereact/dataview';
+import { ListItem, GridItem, Card, CardPopUp } from './CardItem';
 
-import { ListItem, GridItem, Card, CardPopUp } from "./CardItem";
-import FilterComponent from "./FilterComponent";
-
-interface SortOption {
-  label: string;
-  value: string;
-}
 
 export default function CardView() {
   const [cards, setCards] = useState<Card[]>([]);
   const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
   const [popCard, setPopCard] = useState<Card | undefined>();
-  const [layout, setLayout] = useState<'grid' | 'list' | (string & Record<string, unknown>)>('grid');
-  const [sortKey, setSortKey] = useState<string>('cost');
-  const [, setSortOrder] = useState<number>(1);
+  const [layout] = useState<'grid' | 'list' | (string & Record<string, unknown>)>('grid');
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
   const scrollContainerRef = useRef<HTMLDivElement | null>(null);
 
-  const sortOptions: SortOption[] = [
-    { label: 'Cost: High to Low', value: '!cost' },
-    { label: 'Cost: Low to High', value: 'cost' },
-  ];
-
   useEffect(() => {
-    // Initialize with limit and skip set to 25 and 0, respectively
+    // Initialize with limit and skip
     loadInitialCards(50, 0);
   }, []);
 
@@ -79,7 +65,6 @@ export default function CardView() {
         });
   };
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   const handleScroll = () => {
     if (loading || !hasMore) return;
 
@@ -99,7 +84,6 @@ export default function CardView() {
 
     return () => {
       if (scrollContainerRef.current) {
-        // eslint-disable-next-line react-hooks/exhaustive-deps
         scrollContainerRef.current.removeEventListener('scroll', handleScroll);
       }
     };
@@ -129,34 +113,12 @@ export default function CardView() {
     }
   };
 
-  
-
-  const onSortChange = (event: DropdownChangeEvent) => {
-    const value = event.value;
-    setSortKey(value);
-    setSortOrder(1);
-    setCards([]);
-    setHasMore(true);
-    loadInitialCards(25, 0);
-  };
-
+  // Remember to fix this issue in navbar.tsx!
+  document.body.style.overflow = "hidden";
   return (
-    <div className="card">
-      <DataView
-        value={cards}
-        itemTemplate={itemTemplate}
-        sortField={sortField}
-        sortOrder={sortOrder}
-        layout={layout}
-        header={FilterComponent()}
-      />
-      {popCard && (
-        <CardPopUp
-          card={popCard}
-          open={isDialogOpen}
-          onClose={closeDialog}
-        ></CardPopUp>
-      )}
-    </div>
+      <div className="card" ref={scrollContainerRef} style={{ overflow: 'auto', height: '783px' }}>
+        <DataView value={cards} itemTemplate={itemTemplate} layout={layout}/>
+        {popCard && <CardPopUp card={popCard} open={isDialogOpen} onClose={closeDialog} />}
+      </div>
   );
 }
