ace5e0c063e846022d98f988f8366c4b5ffce14e
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2023-11-21 15:54:45 +0100

    Added functionality to deckService.ts

---
+++ client/src/service/DeckService.ts
@@ -0,0 +1,97 @@
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/Hearthstone",
+  cache: new InMemoryCache(),
+});
+
+// DeckService will have three functions, getCardsInDeck, createDeck and deleteDeck
+// getCardsInDeck will take in a deckId and return a list of cards
+// createDeck will take in a string and return deckId
+// deleteDeck will take in a deckId and return a boolean
+
+export const DeckService = {
+    getDecks: async () => {
+        const { data } = await client.query({
+            query: gql`
+            query Decks {
+        user {
+            decks {
+            deckName
+            id
+            } 
+        }
+        }`
+        })
+        return data.user.decks;},
+    getCardsInDeck: async (getCardsInDeckId: string) => {
+        const { data } = await client.query({
+        query: gql`
+            query GetCardsInDeck($getCardsInDeckId: ID!) {
+        getCardsInDeck(id: $getCardsInDeckId) {
+            id
+            cardId
+            dbfId
+            name
+            cardSet
+            type
+            text
+            playerClass
+            locale
+            faction
+            mechanics {
+            name
+            }
+            cost
+            attack
+            health
+            flavour
+            artist
+            elite
+            rarity
+            spellSchool
+            race
+            img
+            durability
+            collectible
+            imgGold
+            otherRaces
+            howToGetSignature
+            armor
+            howToGet
+            howToGetGold
+            howToGetDiamond
+            multiClassGroup
+            classes
+        }
+        }
+        `,
+        variables: { getCardsInDeckId },
+        });
+        return data.getCardsInDeck;
+    },
+    createDeck: async (deckName: string) => {
+        const { data } = await client.mutate({
+        mutation: gql`
+            mutation CreateDeck($deckName: String!) {
+                createDeck(deckName: $deckName) {
+                    id
+        }
+        }
+        `,
+        variables: { deckName },
+        });
+        return data.createDeck.id;
+    },
+    deleteDeck: async (deckId: string) => {
+        const { data } = await client.mutate({
+        mutation: gql`
+            mutation DeleteDeck($deckId: String!) {
+            deleteDeck(deckId: $deckId)
+            }
+        `,
+        variables: { deckId },
+        });
+        return data.deleteDeck;
+    },
+    };
