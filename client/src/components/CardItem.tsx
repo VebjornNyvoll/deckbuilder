f08e5191c3a9bf5a092b6ba763e6bb684cfbef17
Sigve Johan Hov <sigvejh@stud.ntnu.no>
2023-11-22 11:34:27 +0100

    [Type]: Run prettier [Optional Body explaining the change in more detail]: Run prettier on CardItem.tsx and CardOverlayComponent.tsx [References or Issue Numbers]: #28

--- client/src/components/CardItem.tsx
+++ client/src/components/CardItem.tsx
@@ -3,7 +3,7 @@ import { Tag } from "primereact/tag";
 import { Button } from "primereact/button";
 import CardTooltip from "./CardTooltip";
 import { Dialog } from "primereact/dialog";
-import {CardOverlayComponent} from "./CardOverlayComponent";
+import { CardOverlayComponent } from "./CardOverlayComponent";
 import { OverlayPanel } from "primereact/overlaypanel";
 import { useAppSelector } from "../service/hooks";
 import { CardService } from "../service/CardService";
@@ -62,8 +62,11 @@ const getSeverity = (card: Card) => {
   }
 };
 
-export function CardPopUp(props: { cardId: string; open: boolean; onClose: () => void }) {
-
+export function CardPopUp(props: {
+  cardId: string;
+  open: boolean;
+  onClose: () => void;
+}) {
   const { open, onClose, cardId } = props;
   const [card, setCard] = useState(CardService.getEmptyCard());
 
@@ -81,43 +84,45 @@ export function CardPopUp(props: { cardId: string; open: boolean; onClose: () =>
     if (!card) return <p>Loading card details...</p>;
   };
 
-  const headerContent = <div style={{ textAlign: "center" }}>{card ? card.name : "Loading..."}</div>;
+  const headerContent = (
+    <div style={{ textAlign: "center" }}>{card ? card.name : "Loading..."}</div>
+  );
 
   return (
-      <Dialog
-          headerStyle={{ height: "70px" }}
-          header={headerContent}
-          visible={open}
-          style={{ width: "60vw", maxWidth: "400px" }}
-          onHide={onClose}
-      >
-        <div
-            tabIndex={0} style={{ textAlign: "left" }}>
-          {card && (
-              <>
-                {card.attack && <p>Attack: {card.attack}</p>}
-                {card.health && <p>Health: {card.health}</p>}
-                {card.cost != null && <p>Cost: {card.cost}</p>}
-                {card.rarity && <p>Rarity: {card.rarity}</p>}
-                {card.text && <p style={{ whiteSpace: "pre-wrap" }}>
-                  Text: {parse(card.text .replace(/\[x]|#|\$|\\n/g, " "))}
-                </p>}
-                {card.flavor && <p>Flavor: {card.flavor}</p>}
-                {card.faction && <p>Faction: {card.faction}</p>}
-                {card.cardSet && <p>Card set: {card.cardSet}</p>}
-                {card.playerClass && <p>Player class: {card.playerClass}</p>}
-                {card.artist && <p>Artist: {card.artist}</p>}
-                <br />
-                {renderCardProperties()}
-              </>
-          )}
-        </div>
-      </Dialog>
+    <Dialog
+      headerStyle={{ height: "70px" }}
+      header={headerContent}
+      visible={open}
+      style={{ width: "60vw", maxWidth: "400px" }}
+      onHide={onClose}
+    >
+      <div tabIndex={0} style={{ textAlign: "left" }}>
+        {card && (
+          <>
+            {card.attack && <p>Attack: {card.attack}</p>}
+            {card.health && <p>Health: {card.health}</p>}
+            {card.cost != null && <p>Cost: {card.cost}</p>}
+            {card.rarity && <p>Rarity: {card.rarity}</p>}
+            {card.text && (
+              <p style={{ whiteSpace: "pre-wrap" }}>
+                Text: {parse(card.text.replace(/\[x]|#|\$|\\n/g, " "))}
+              </p>
+            )}
+            {card.flavor && <p>Flavor: {card.flavor}</p>}
+            {card.faction && <p>Faction: {card.faction}</p>}
+            {card.cardSet && <p>Card set: {card.cardSet}</p>}
+            {card.playerClass && <p>Player class: {card.playerClass}</p>}
+            {card.artist && <p>Artist: {card.artist}</p>}
+            <br />
+            {renderCardProperties()}
+          </>
+        )}
+      </div>
+    </Dialog>
   );
 }
 
 export const ListItem: React.FC<CardItemProps> = ({ card, onClick }) => {
-
   const dataSaver = useAppSelector((state) => state.datasaver.datasaver);
   const idString = card.id.toString();
   const op = useRef(null);
@@ -136,137 +141,170 @@ export const ListItem: React.FC<CardItemProps> = ({ card, onClick }) => {
     }
   };
 
-  return(
-      <div className="col-12 border-1 surface-border surface-card border-round ">
-        <button onClick={handleItemClick} aria-haspopup
-            aria-labelledby={idString + "set " + idString + "faction " + idString + "name"}
-            className="col-12 border-1 surface-border surface-card border-round ">
-          <div className="flex rem flex-row xl:align-items-start p-4 gap-4">
-            {!dataSaver && (<img
-                className="w-10rem shadow-2 block xl:block mx-auto border-round align-items-center"
-                src={card.img}
-                alt={card.name}
-                />
-            )}
-            <div
-                className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
-              <div className="flex flex-column align-items-center sm:align-items-start gap-3">
-                <div id={idString + "name"} className="text-2xl font-bold text-900">{card.name}</div>
-                {/* Additional card details */}
-                <div className="flex align-items-center gap-3">
-                  <span className="flex align-items-center gap-2">
-                    <i className="pi pi-book"></i>
-                    <span className="font-semibold">{card.cardSet}</span>
-                  </span>
-                  <Tag
-                      value={card.faction ? card.faction.toString() : "None"}
-                      severity={getSeverity(card)}>
-                  </Tag>
+  return (
+    <div className="col-12 border-1 surface-border surface-card border-round ">
+      <button
+        onClick={handleItemClick}
+        aria-haspopup
+        aria-labelledby={
+          idString + "set " + idString + "faction " + idString + "name"
+        }
+        className="col-12 border-1 surface-border surface-card border-round "
+      >
+        <div className="flex rem flex-row xl:align-items-start p-4 gap-4">
+          {!dataSaver && (
+            <img
+              className="w-10rem shadow-2 block xl:block mx-auto border-round align-items-center"
+              src={card.img}
+              alt={card.name}
+            />
+          )}
+          <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+            <div className="flex flex-column align-items-center sm:align-items-start gap-3">
+              <div
+                id={idString + "name"}
+                className="text-2xl font-bold text-900"
+              >
+                {card.name}
+              </div>
+              {/* Additional card details */}
+              <div className="flex align-items-center gap-3">
+                <span className="flex align-items-center gap-2">
+                  <i className="pi pi-book"></i>
+                  <span className="font-semibold">{card.cardSet}</span>
+                </span>
+                <Tag
+                  value={card.faction ? card.faction.toString() : "None"}
+                  severity={getSeverity(card)}
+                ></Tag>
+              </div>
+
+              {dataSaver && (
+                <div>
+                  <p className="p-0 m-0">
+                    {card.attack
+                      ? "Attack: " + card.attack.toString()
+                      : "No attack"}
+                  </p>
+                  <p>
+                    {card.health
+                      ? "Health: " + card.health.toString()
+                      : "No health"}
+                  </p>
                 </div>
+              )}
+            </div>
 
-                {dataSaver && (
-                    <div>
-                      <p className="p-0 m-0">
-                        {card.attack
-                            ? "Attack: " + card.attack.toString()
-                            : "No attack"}
-                      </p>
-                      <p>
-                        {card.health
-                            ? "Health: " + card.health.toString()
-                            : "No health"}
-                      </p>
-                    </div>
-                )}
-              </div>
-
-              <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
-                {<span className="text-2xl font-semibold">
+            <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
+              {
+                <span className="text-2xl font-semibold">
                   {card.type ? card.type.toString() : "No Type"}
-                </span>}
-                <Button
-                    id={idString + "btn"}
-                    title={"Add Card: " + card.name.toString()}
-                    aria-labelledby={idString + "btn"}
-                    icon="pi pi-plus"
-                    className="p-button-rounded"
-                    onClick={showOverlayPanel}
-                />
-              </div>
+                </span>
+              }
+              <Button
+                id={idString + "btn"}
+                title={"Add Card: " + card.name.toString()}
+                aria-labelledby={idString + "btn"}
+                icon="pi pi-plus"
+                className="p-button-rounded"
+                onClick={showOverlayPanel}
+              />
             </div>
           </div>
-        </button>
+        </div>
+      </button>
 
-        <OverlayPanel ref={op} dismissable showCloseIcon={true}>
-          <CardOverlayComponent cardId={card.id}/>
-        </OverlayPanel>
-      </div>
-  )
+      <OverlayPanel ref={op} dismissable showCloseIcon={true}>
+        <CardOverlayComponent cardId={card.id} />
+      </OverlayPanel>
+    </div>
+  );
 };
 
-
 export const GridItem: React.FC<CardItemProps> = ({ card, onClick }) => {
+  const dataSaver = useAppSelector((state) => state.datasaver.datasaver);
+  const idString = card.id.toString();
+  const op = useRef(null);
+  const showOverlayPanel = (event: React.SyntheticEvent<Element, Event>) => {
+    // Prevent event from bubbling up to parent elements
+    event.stopPropagation();
+    if (op.current && (op.current as OverlayPanel).toggle) {
+      (op.current as OverlayPanel).toggle(event);
+    }
+  };
 
-  const dataSaver = useAppSelector((state) => state.datasaver.datasaver), idString = card.id.toString(),
-      op = useRef(null), showOverlayPanel = (event: React.SyntheticEvent<Element, Event>) => {
-        // Prevent event from bubbling up to parent elements
-        event.stopPropagation();
-        if (op.current && (op.current as OverlayPanel).toggle) {
-          (op.current as OverlayPanel).toggle(event);
-        }
-      }, handleItemClick = () => {
-        if (onClick) {
-          onClick(card.id);
-        }
-      };
+  //The onClick as defined in Props must take in an argument card
+  const handleItemClick = () => {
+    if (onClick) {
+      onClick(card.id);
+    }
+  };
 
   return (
     <div className="col-12 sm:col-6 lg:col-4 xl:col-3 p-2">
-      <button style={{width: "100%"}} className="p-4 border-1 surface-border surface-card border-round" aria-haspopup aria-labelledby={idString+"set "+idString+"faction "+idString+"name"} role="button" onClick={handleItemClick}>
-        <div  className="flex align-items-center justify-content-between gap-2">
+      <button
+        style={{ width: "100%" }}
+        className="p-4 border-1 surface-border surface-card border-round"
+        aria-haspopup
+        aria-labelledby={
+          idString + "set " + idString + "faction " + idString + "name"
+        }
+        role="button"
+        onClick={handleItemClick}
+      >
+        <div className="flex align-items-center justify-content-between gap-2">
           <div className="flex align-items-center gap-2">
             <i className="pi pi-book"></i>{" "}
-            <span id={idString+"set"} className="font-semibold text-xs">{card.cardSet}</span>
+            <span id={idString + "set"} className="font-semibold text-xs">
+              {card.cardSet}
+            </span>
           </div>
-          <Tag id={idString+"faction"}
-               value={card.faction ? card.faction.toString() : "None"}
-               severity={getSeverity(card)}>
-          </Tag>
+          <Tag
+            id={idString + "faction"}
+            value={card.faction ? card.faction.toString() : "None"}
+            severity={getSeverity(card)}
+          ></Tag>
         </div>
 
         <div className="flex flex-column align-items-center gap-3 py-5">
           {!dataSaver && (
-              <img
-                  className={"w-9 shadow-2 border-round"}
-                  src={card.img}
-                  alt={card.name}
-              />
+            <img
+              className={"w-9 shadow-2 border-round"}
+              src={card.img}
+              alt={card.name}
+            />
           )}
           <div className="text-l font-bold">{card.name}</div>
           {dataSaver && (
-              <>
-                <CardTooltip card={card}></CardTooltip>
-                <p className="p-0 m-0 align-items-center">
-                  {card.attack ? "Attack: " + card.attack.toString() : "No attack"}
-                </p>
-                <p>
-                  {card.health ? "Health: " + card.health.toString() : "No health"}
-                </p>
-              </>
+            <>
+              <CardTooltip card={card}></CardTooltip>
+              <p className="p-0 m-0 align-items-center">
+                {card.attack
+                  ? "Attack: " + card.attack.toString()
+                  : "No attack"}
+              </p>
+              <p>
+                {card.health
+                  ? "Health: " + card.health.toString()
+                  : "No health"}
+              </p>
+            </>
           )}
         </div>
 
         <div className="flex align-items-center justify-content-between">
-          { <span className="text-2xl font-semibold">
-            {card.type ? card.type.toString() : "No Type"}
-          </span> }
+          {
+            <span className="text-2xl font-semibold">
+              {card.type ? card.type.toString() : "No Type"}
+            </span>
+          }
           <Button
-              id = {idString+"btn"}
-              title={"Add Card: "+ card.name.toString()}
-              aria-labelledby={idString+"btn"}
-              icon="pi pi-plus"
-              className="p-button-rounded"
-              onClick={showOverlayPanel}
+            id={idString + "btn"}
+            title={"Add Card: " + card.name.toString()}
+            aria-labelledby={idString + "btn"}
+            icon="pi pi-plus"
+            className="p-button-rounded"
+            onClick={showOverlayPanel}
           />
         </div>
       </button>
--- client/src/components/CardOverlayComponent.tsx
+++ client/src/components/CardOverlayComponent.tsx
@@ -1,55 +1,61 @@
-import React, { useRef } from 'react';
-import { Toast } from 'primereact/toast';
-import { Column } from 'primereact/column';
-import { DataTable } from 'primereact/datatable';
-import { Button } from 'primereact/button';
-import { gql, useMutation, useQuery } from '@apollo/client';
-import { useContext } from 'react';
-import { AuthContext } from '../context/authContext';
+import { useRef } from "react";
+import { Toast } from "primereact/toast";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { Button } from "primereact/button";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { useContext } from "react";
+import { AuthContext } from "../context/authContext";
 
-export const CardOverlayComponent = ({cardId }) => {
+export const CardOverlayComponent = ({ cardId }) => {
   const context = useContext(AuthContext);
 
   const CARD_TO_DECK = gql`
-  mutation AddCards($cardIds: [ID!]!, $deckId: ID!) {
-    addCards(cardIds: $cardIds, deckId: $deckId) {
-      error {
-        error
-        message
+    mutation AddCards($cardIds: [ID!]!, $deckId: ID!) {
+      addCards(cardIds: $cardIds, deckId: $deckId) {
+        error {
+          error
+          message
+        }
       }
     }
-  }`;
+  `;
 
   const GET_DECKS = gql`
-  query Decks {
-    user {
-      decks {
-        deckName
-        id
-      } 
+    query Decks {
+      user {
+        decks {
+          deckName
+          id
+        }
+      }
     }
-  }`;
+  `;
 
   const { loading, error, data } = useQuery(GET_DECKS);
-  const decks = data?.user?.decks?.map(deck => ({ deckId: deck.id, name: deck.deckName })) || [];
+  const decks =
+    data?.user?.decks?.map((deck) => ({
+      deckId: deck.id,
+      name: deck.deckName,
+    })) || [];
 
   const toast = useRef(null);
-  
+
   const showMessage = (isError, content) => {
     toast.current.replace({
-      severity: isError ? 'error' : 'success',
-      summary: isError ? 'Error' : 'Success',
+      severity: isError ? "error" : "success",
+      summary: isError ? "Error" : "Success",
       detail: content,
-      life: 3000
+      life: 3000,
     });
   };
 
   const [addCardsToDeck] = useMutation(CARD_TO_DECK);
 
   const addButtonTemplate = (rowData) => (
-    <Button 
-      label="Add" 
-      className="p-button-sm" 
+    <Button
+      label="Add"
+      className="p-button-sm"
       onClick={(e) => handleAddButtonClick(e, rowData)}
     />
   );
@@ -60,11 +66,11 @@ export const CardOverlayComponent = ({cardId }) => {
       const response = await addCardsToDeck({
         variables: {
           cardIds: [cardId],
-          deckId: rowData.deckId
+          deckId: rowData.deckId,
         },
         context: {
           headers: {
-            Authorization: `Bearer ${context?.token}` // Adjust as per your auth context structure
+            Authorization: `Bearer ${context?.token}`,
           },
         },
       });
@@ -83,14 +89,32 @@ export const CardOverlayComponent = ({cardId }) => {
   if (error) return <p>Error loading decks</p>;
 
   const nameBodyTemplate = (rowData) => (
-    <span style={{ cursor: 'pointer' }}>{rowData.name}</span>
+    <span style={{ cursor: "pointer" }}>{rowData.name}</span>
   );
 
   return (
-    <div className="card flex flex-column align-items-center gap-3" style={{ width: '100%', maxWidth: '30rem', overflowY: 'auto', maxHeight: '400px' }}>
+    <div
+      className="card flex flex-column align-items-center gap-3"
+      style={{
+        width: "100%",
+        maxWidth: "30rem",
+        overflowY: "auto",
+        maxHeight: "400px",
+      }}
+    >
       <Toast ref={toast} />
-      <DataTable sortOrder={-1} sortField={"name"} value={decks} tableStyle={{ width: '100%', maxWidth: '30rem' }}>
-        <Column sortable field="name" header="Deck" body={nameBodyTemplate}></Column>
+      <DataTable
+        sortOrder={-1}
+        sortField={"name"}
+        value={decks}
+        tableStyle={{ width: "100%", maxWidth: "30rem" }}
+      >
+        <Column
+          sortable
+          field="name"
+          header="Deck"
+          body={nameBodyTemplate}
+        ></Column>
         <Column body={addButtonTemplate}></Column>
       </DataTable>
     </div>
