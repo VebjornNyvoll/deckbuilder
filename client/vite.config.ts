f49084f32d7b4965f523b9ab10cfe6201b3caf40
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2023-11-22 23:46:26 +0100

    Updated vite config basename

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/project2',
+  base: '/project2/',
   plugins: [react(), viteMockPlugin()],
   test: {
     // Put your Vitest options here
