9815ef542b1ec480dc70d6ae951c93f01da9bbcd
Sigve Johan Hov <sigvejh@stud.ntnu.no>
2023-11-28 18:32:59 +0100

    Starting to change E2E to work with VM instead of Localhost References #42

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   projectId: 'cvqn9t',
+  retries: 4,
   e2e: {
+    baseUrl:'http://it2810-66.idi.ntnu.no/project2/',
     experimentalStudio: true,
     setupNodeEvents() {
       // implement node event listeners here
